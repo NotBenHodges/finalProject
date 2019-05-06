cb937c10334b98fd9a74ab11364e864d439fd249
Ben Hodges <ben.hodges@centre.edu>
2019-05-06 14:33:29 -0400

    Drew a county map but still buggy

--- maps.js
+++ maps.js
@@ -1,10 +1,15 @@
 var geoP = d3.json('USStates5m.json')
 var stateP = d3.csv('justStates.csv')
+var geo2P = d3.json('USCounties5m.json')
+var state2P = d3.csv('statesAndCounties.csv')
 
-Promise.all([geoP,stateP]).then(function(values){
+Promise.all([geoP,stateP,geo2P,state2P]).then(function(values){
   var geoData = values[0];
   var stateData = values[1];
+  var geoData2 = values[2];
+  var stateData2 = values[3];
   drawMap(geoData,stateData)
+  drawMap2(geoData2,stateData2)
 });
 
 var h = 600;
@@ -74,7 +79,7 @@ var drawMap = function(geoData,stateData){
           return color(str);
         })
         .on('mouseover', function(d){
-          
+
         })
 
   document.getElementById('allPop').onclick = function(d){
@@ -136,20 +141,117 @@ document.getElementById('median').onclick = function(d){
   document.getElementById('allPop').onmouseover = function(d){
     console.log('hello Ben');
   };
+}
+
+var drawMap2 = function(geoData,stateData){
+
+  var projection = d3.geoAlbersUsa()
+                      .translate([w/2,h/2]);
+
+  var stateGenerator = d3.geoPath()
+                          .projection(projection);
+
+  var stateDict = {}
+
+  var stateDict2 = {}
+
+  stateData.forEach(function(state){
+    stateDict[state.Name.trim()]=state.Postal;
+    stateDict[state.Postal.trim()]=state.poverty;
+    stateDict[state.FIPS.trim()]=state.AllAges;
+    stateDict2[state.FIPS.trim()]=state.Median;
+  });
+  console.log(stateDict2);
+
+  geoData.features.forEach(function(state){
+    state.properties.ABBR = stateDict[state.properties.NAME]
+    state.properties.ESTIMATE = stateDict[state.properties.ABBR]
+    state.properties.ESTIMATE2 = stateDict[state.properties.STATE]
+    state.properties.MEDIAN = stateDict2[state.properties.STATE]
+    console.log(state.properties.MEDIAN);
+  });
+  console.log(geoData)
+
+  var svg = d3.select('.s')
+              .attr('height',h)
+              .attr('width',w);
+
+  var counties = svg.append('g')
+                  .attr('id','counties')
+                  .selectAll('g')
+                  .data(geoData.features)
+                  .enter()
+                  .append('g')
+                  .classed('county',true);
 
 
-/*
-  var svg2 = d3.select('body')
-                .append('svg')
-                .attr('height',100)
-                .attr('width',100);
+  var color = d3.scaleSequential(d3.interpolateGreens)
+                .domain([0,4000000]);
 
-  var xScale = d3.scaleLinear()
-                  .domain([0,56])
-                  .range([0,100])
+  var color2 = d3.scaleSequential(d3.interpolateGreens)
+                  .domain([7,20]);
 
-  var yScale = d3.scaleLinear()
-                  .domain([0,4,080,377])
-                  .range([0,100])*/
+  var color3 = d3.scaleSequential(d3.interpolateGreens)
+                  .domain([40000,85000]);
 
+
+  counties.append('path')
+        .attr('d',stateGenerator)
+        .attr('stroke','green')
+        .attr('fill', function(d){
+          //console.log(parseInt(d.poverty))
+          var str = d.properties.ESTIMATE
+          str = str.replace(/,/g,"")
+          str = parseInt(str)
+          console.log(str)
+          return color(str);
+        })
+        .on('mouseover', function(d){
+
+        })
+
+  document.getElementById('allPop2').onclick = function(d){
+    document.getElementById('title').innerHTML = 'Poverty by Population Size'
+    counties.append('path')
+          .attr('d',stateGenerator)
+          .attr('stroke','green')
+          .attr('fill', function(d){
+            //console.log(parseInt(d.poverty))
+            var str = d.properties.ESTIMATE
+            str = str.replace(/,/g,"")
+            str = parseInt(str)
+            //console.log(str)
+            return color(str);
+          });
+  };
+
+document.getElementById('allPer2').onclick = function(d){
+  document.getElementById('title').innerHTML = 'Poverty by Percentage'
+  counties.append('path')
+        .attr('d',stateGenerator)
+        .attr('stroke','green')
+        .attr('fill', function(d){
+          //console.log(parseInt(d.poverty))
+          var str = d.properties.ESTIMATE2
+          str = str.replace(/,/g,"")
+          str = parseInt(str)
+          //console.log(str)
+          return color2(str);
+        });
+};
+
+document.getElementById('median2').onclick = function(d){
+  document.getElementById('title').innerHTML = 'Median Household Income'
+  counties.append('path')
+        .attr('d',stateGenerator)
+        .attr('stroke','green')
+        .attr('fill', function(d){
+          //console.log(parseInt(d.poverty))
+          var str = d.properties.MEDIAN
+          str = str.replace(/,/g,"")
+          str = parseInt(str)
+          //console.log(str)
+          return color3(str);
+        });
+};
 }
